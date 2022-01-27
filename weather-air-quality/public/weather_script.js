a79a9483de234291147ca63c27d77beaa9009299
Vikram Negi <viknegi0@gmail.com>
2022-01-27 22:51:07 +0530

    leaflet.js with div-marker

--- weather-air-quality/public/weather_script.js
+++ weather-air-quality/public/weather_script.js
@@ -5,9 +5,14 @@ import getData from "./getData.js";
 // Geo Location
 const latitude = document.getElementById("lat");
 const longitude = document.getElementById("lon");
-const weatherBtn = document.getElementById("get-weather");
 
-navigator.geolocation.getCurrentPosition( async pos => {
+const map = L.map('map').setView([0, 0], 3);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution }).addTo(map);
+
+navigator.geolocation.getCurrentPosition(async pos => {
 
   const coords = pos.coords;
   // console.log(coords);
@@ -21,4 +26,26 @@ navigator.geolocation.getCurrentPosition( async pos => {
   const data = await getData(lat, lon);
   console.log(data);
 
-});
+  const myIcon = L.divIcon({
+    html: `<img src="${data.weatherIconPath}" /><span>${data.city}<span>`,
+    iconAnchor: [25, 25],
+    popupAnchor: [35, -5],
+  });
+
+  const your_marker = L.marker([lat, lon], {
+    keyboard: true,
+    icon: myIcon
+  }).addTo(map);
+  your_marker.bindPopup(`
+    <p class="data">
+      <b>City:</b> ${data.city}<br />
+      <b>Temperature:</b> ${data.temp}°C<br />
+      <b>Humidity:</b> ${data.humidity}%<br />
+      <b>Air Quality:</b> ${data.airQuality} ${data.airQualityUnit}<br />
+      <b>Wind Speed:</b> ${data.windSpeed} km/hr<br />
+    </p>
+  `);
+
+  map.setView([lat, lon], 5);
+
+}, (err) => console.error(err));
