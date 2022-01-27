20e06af343e3bed84e54359a48e43e8ef74720d6
Vikram Negi <viknegi0@gmail.com>
2022-01-27 13:46:17 +0530

    front end modular code

---
+++ weather-air-quality/public/getData.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const getData = async (lat, lon) => {
+
+  try {
+
+    const info = {};
+    const res = await fetch(`/weather?lat=${lat}&lon=${lon}`);
+    const [weatherData, aqData, weatherImg] = await res.json();
+
+    console.log(weatherData, aqData, weatherImg);
+
+    const temp_info = weatherData;
+
+    if (temp_info.failed) {
+
+      console.log(temp_info.message);
+      info.tempDataFailed = true;
+
+    } else {
+
+      info.temp = temp_info.main.temp;
+      info.humidity = temp_info.main.humidity;
+      info.windSpeed = temp_info.wind.speed;
+
+    }
+
+    const air_info = aqData;
+
+    if (air_info.failed) {
+
+      console.log(air_info.message);
+      info.airDataFailed = true;
+
+    } else {
+
+      // results are sorted by nearest
+      const air_quality = air_info.results[0];
+      const first_measurement = air_quality.measurements.filter(reading => reading.parameter === "pm10")[0];
+
+      // console.log(first_measurement);
+
+      info.location = air_quality.location;
+      info.city = air_quality.city;
+      info.airQuality = first_measurement.value;
+      info.airQualityUnit = first_measurement.unit || "µg/m³";
+      info.lastUpdated = new Date(first_measurement.lastUpdated).toLocaleString();
+
+    }
+
+    return info;
+
+  } catch (err) {
+    console.error(err);
+    console.warn("All promises failed!");
+    return info;
+  }
+
+}
+
+export default getData;
